be323b3aaabd3b9f2c659c3a99e0e4594724818a
nikishkaa <nikita.goutor@gmail.com>
2024-04-09 20:32:21 +0300

    Add read method.

--- src/by/itstep/goutor/javalesson/lesson39/controller/BinaryWorker.java
+++ src/by/itstep/goutor/javalesson/lesson39/controller/BinaryWorker.java
@@ -1,9 +1,6 @@
 package by.itstep.goutor.javalesson.lesson39.controller;
 
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
+import java.io.*;
 
 public class BinaryWorker {
     public static void write(String fileName) {
@@ -12,10 +9,9 @@ public class BinaryWorker {
         try {
             stream = new FileOutputStream(fileName);
 
-            for (int i = 0; i < 100; i++) {
+            for (int i = -1000; i < 0; i++) {
                 stream.write(i);
             }
-
 
             stream.flush();
 
@@ -29,5 +25,32 @@ public class BinaryWorker {
         }
 
 
+    }
+
+
+    public static String read(String fileName) {
+
+        InputStream stream = null;
+        StringBuilder builder = new StringBuilder();
+        try {
+            stream = new FileInputStream(fileName);
+
+            int number;
+
+            while ((number = stream.read()) != -1) {
+                builder.append(number).append(" ");
+            }
+
+
+            stream.close();
+
+
+        } catch (FileNotFoundException exception) {
+        } catch (IOException exception) {
+            System.out.println(exception);
+
+        }
+
+        return builder.toString();
     }
 }
--- src/by/itstep/goutor/javalesson/lesson39/controller/Main.java
+++ src/by/itstep/goutor/javalesson/lesson39/controller/Main.java
@@ -7,7 +7,10 @@ public class Main {
         String binaryFileName = "D:/Прога/test.byn";
 
 
-        BinaryWorker.write(binaryFileName);
+//        BinaryWorker.write(binaryFileName);
+
+        String text = BinaryWorker.read(binaryFileName);
+        System.out.println(text);
 
     }
 }
