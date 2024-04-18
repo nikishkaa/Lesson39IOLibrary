3697942f881608641c283e2a0e5bf000c9a80671
nikishkaa <nikita.goutor@gmail.com>
2024-04-18 10:50:49 +0300

    Lesson 40.

---
+++ src/by/itstep/goutor/javalesson/lesson39/logic/entity/Student.java
@@ -0,0 +1,12 @@
+package by.itstep.goutor.javalesson.lesson39.logic.entity;
+
+public class Student {
+    private String name;
+    private int age;
+    private double mark;
+    private boolean alive;
+
+    public Student() {
+    }
+
+}
